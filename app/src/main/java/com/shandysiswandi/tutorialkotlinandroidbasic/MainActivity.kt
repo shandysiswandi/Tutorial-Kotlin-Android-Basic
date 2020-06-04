985a290ffd698607ac6b2707e82dbdb159de14ad
shandysiswandi <shandysiswandi@gmail.com>
2020-06-05 00:46:56 +0700

    Initial commit

---
+++ app/src/main/java/com/shandysiswandi/tutorialkotlinandroidbasic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shandysiswandi.tutorialkotlinandroidbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
